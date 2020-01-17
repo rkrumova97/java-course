6b9d6abf2e6266a41850f3eb29d2884d1cc91bca
Hudson <denesioanag@yahoo.com>
2020-01-17 16:24:19 +0100

    Add the option, registration, story, preferences

---
+++ restorant/app/src/main/java/com/restaurantapp/Activity_Option.java
@@ -0,0 +1,13 @@
+package com.restaurantapp;
+
+import android.support.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity_Option extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity__option);
+    }
+}
